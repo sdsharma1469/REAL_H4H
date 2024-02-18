00aa0540f4da3192379bb98a39856cac4c1e2d38
sdsharma1469 <sdsharma@scu.edu>
2024-02-17 18:59:35 -0800

    a lottt

--- APIS/app.py
+++ APIS/app.py
@@ -1,23 +1,30 @@
 from flask import Flask, jsonify, request
 from readJson import print_products, count_products, add_product
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+CORS(app, origins='*')
+
 
 @app.route('/print', methods=['GET'])
 def printing():
     return print_products()
 
+
 @app.route('/add', methods=['POST'])
 def add():
-    data = request.json  # Assuming the data is sent as JSON
-    param1 = data.get('productName')
-    param2 = data.get('productPrice')
-    param3 = data.get('productQuantity')
-    param4 = data.get('productContact')  # Corrected the parameter name
-    # Assuming add_product takes four parameters
-    add_product(param1, param2, param3, param4)
-    return jsonify({'message': 'Product added successfully'})
+    data = request.json
+    productName = data.get('productName')
+    productPrice = data.get('productPrice')
+    productQuantity = data.get('productQuantity')
+    productContact = data.get('productContact')
+    
+    print(productQuantity)
+    add_product(productName, productPrice, productQuantity, productContact)
+    return "Parameters received"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
