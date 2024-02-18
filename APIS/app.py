0a18b490faca4c164dbbd438b8587d745253e002
sdsharma1469 <sdsharma@scu.edu>
2024-02-17 16:54:32 -0800

    xx

--- APIS/app.py
+++ APIS/app.py
@@ -4,28 +4,20 @@ from readJson import print_products, count_products, add_product
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-
 @app.route('/print', methods=['GET'])
 def printing():
-    return(print_products())
-
-from flask import request
+    return print_products()
 
 @app.route('/add', methods=['POST'])
 def add():
-    
-    param1 = request.form.get('param1')
-    param2 = request.form.get('param2')
-    param3 = request.form.get('param3')
-    param4 = request.form.get('param4')
-
-    return add_product(param1, param2, param3, param4)
-
-
+    data = request.json  # Assuming the data is sent as JSON
+    param1 = data.get('productName')
+    param2 = data.get('productPrice')
+    param3 = data.get('productQuantity')
+    param4 = data.get('productContact')  # Corrected the parameter name
+    # Assuming add_product takes four parameters
+    add_product(param1, param2, param3, param4)
+    return jsonify({'message': 'Product added successfully'})
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# response = requests.get("http://localhost:5000/test")
-# print(response.text)
